de7db849b68cbeee05ba3cb32abd6cd543f3f66e
abdulwariskhan <hellokhan.ch@gmail.com>
2023-09-29 23:34:12 +0200

    Initial Commit

---
+++ WildWood Walks/WildWood_WalksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WildWood_WalksApp.swift
+//  WildWood Walks
+//
+//  Created by Khan on 29.09.23.
+//
+
+import SwiftUI
+
+@main
+struct WildWood_WalksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
