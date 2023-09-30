dcda51ddb14f9181a86f02090f1f6730139b0301
abdulwariskhan <hellokhan.ch@gmail.com>
2023-09-30 07:57:48 +0200

    completed Header Component

--- WildWood Walks/Component/CardView.swift
+++ WildWood Walks/Component/CardView.swift
@@ -12,15 +12,42 @@ struct CardView: View {
         
         ZStack {
             customeBackgroundView()
-            ZStack {
-                
-                Circle()
-                    .fill(LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSamonLight")], startPoint: .topLeading, endPoint: .bottomTrailing))
-                    .frame(width: 256, height: 256)
-                Image("image-1")
-                    .resizable()
-                .scaledToFit()
-            }
+            
+            VStack {
+                // MARK: -HEADER
+                VStack (alignment:.leading) {
+                    HStack {
+                        Text("Walking")
+                            .fontWeight(.black)
+                            .font(.system(size: 40))
+                            .foregroundStyle(LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom))
+                      Spacer()
+                        Button {
+                            //: ACTION
+                            print("Pressed")
+                        } label: {
+                            CustomButtonView()
+                        }
+
+                    }
+                    Text("Fun and enjoy outdoor activity with friends and families")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                } //: HEADER
+                .padding(.horizontal,30)
+                // MARK: - MAIN CONTENT
+                ZStack {
+                    
+                    Circle()
+                        .fill(LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSamonLight")], startPoint: .topLeading, endPoint: .bottomTrailing))
+                        .frame(width: 256, height: 256)
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                // MARK: - FOOTER
+            } //: VSTACK
         } // : CARD SIZE
         .frame(width: 320, height: 570)
     }
